3eebea630f762cfbd85a1db5718d3547793c4aac
Luis Ayala <lguillermoayala@hotmail.com>
2025-03-26 15:27:49 -0600

    correccion

--- src/containers/App.svelte
+++ src/containers/App.svelte
@@ -3,7 +3,7 @@
     import Header from "../components/Header.svelte";
     import Main from "../components/Main.svelte";
     import TimeLine from "../components/TimeLine.svelte";
-    import Sidebar from "../components/Sidebar.svelte";
+    import Sidebar from "../components/SideBar.svelte";
 
     let data = {
 "user": {
